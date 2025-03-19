71999e111da422941f5763025cfbea172f6db35d
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-03-19 22:44:24 +0700

    Feat: add shadcn and theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import ModeToggle from "./components/modeToggle/modeToggle";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <ModeToggle />
       <Image
         src="/images/profile.jpg"
         alt="Picture of the author"
