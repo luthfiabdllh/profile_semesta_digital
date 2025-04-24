fe9cdb1355fa0d627bd9dca2b161e6fc1c91053f
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-04-24 16:37:51 +0700

    Feat: add section image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
         <>
             <Banner headline="Empowering Your Business with Data & AI Solutions" subHeadline="We provide innovative solutions in Big Data, AI, and Digital Transformation to drive your business growth"/>
             <SectionImage background="Vision" title="Our Vision" subtitle="Become a key driver of the digital ecosystem by encouraging innovation and supporting research initiatives." srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
+            <SectionImage imageRight={true} background="Mission" title="Our Vision" subtitle="As a Venture Capital firm, we focus on investing in innovative startups and initiatives to advance the digital ecosystem. We nurture talent, offer expert consulting in Big Data and AI, and help organizations leverage data-driven insights. Additionally, we act as a strategic partner in digital transformation, delivering human-centered solutions that benefit all stakeholders." srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
+            <SectionImage background="Service" title="Our Service" subtitle="At Semesta Data Digital, we drive digital transformation with Venture Capital, Big Data, AI, and ICT Solutions. Our services empower businesses and startups with innovative, data-driven strategies to optimize operations and unlock new opportunities." srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
             <SectionPersuasive sentenceBefore="Transform your business with" highlight="AI-driven insights & Big Data solutions" sentenceAfter=". Make smarter decisions, streamline operations, and drive growth with us!"/>
             <SectionTimeline/>
         </>
--- src/components/components/sectionImage.tsx
+++ src/components/components/sectionImage.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import React from "react";
 
-export default function SectionImage({ background, title, subtitle, srcImage, altImage }: { background?: string, title?: string, subtitle?: string, srcImage?: string, altImage?: string }) {
+
+
+export default function SectionImage({ imageRight, background, title, subtitle, srcImage, altImage }: { background : string, title : string, subtitle : React.ReactNode, srcImage : string, altImage : string, imageRight?: boolean }) {
     return (
-        <div className="relative flex justify-center px-4 items-center mt-8 h-[500px]">
-            <h1 className="absolute hidden sm:block sm:text-[220px] md:text-[300px] lg:text-[350px] text-transparent bg-clip-text bg-gradient-to-b from-indigo-500/50 to-transparent to-75% font-bold text-center z-0">
+        <div className="relative flex  justify-center px-4 items-center mt-8 h-[500px]">
+            <h1 className="absolute hidden sm:block sm:text-[180px] md:text-[240px] lg:text-[280px] text-transparent bg-clip-text bg-gradient-to-b from-indigo-500/50 to-transparent to-75% font-bold text-center z-0">
                 {background}
             </h1>
-            <div className="relative z-10 flex flex-col-reverse md:flex-row items-center gap-12">
+            <div className={`relative z-10 flex flex-col-reverse ${imageRight === true ? "md:flex-row" : "md:flex-row-reverse"} items-center gap-12`}>
                 <div className="text-center md:text-left">
                     <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">{title}</h2>
                     <p className="text-neutral-600 dark:text-neutral-400 mt-6 max-w-md">{subtitle}</p>
