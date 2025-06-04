0951e77ae7a52b1ede1867cdbb5326fd3ee57d81
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-06-05 02:37:14 +0700

    fix: fix page about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,33 @@ import { SectionTimeline } from "@/components/components/sectionTimeline";
 export default function About() {
     return (
         <>
-            <SectionImage background="Vision" title="Our Vision" subtitle="Become a key driver of the digital ecosystem by encouraging innovation and supporting research initiatives." srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
-            <SectionImage imageRight={true} background="Mission" title="Our Mission" subtitle="As a Venture Capital firm, we focus on investing in innovative startups and initiatives to advance the digital ecosystem. We nurture talent, offer expert consulting in Big Data and AI, and help organizations leverage data-driven insights. Additionally, we act as a strategic partner in digital transformation, delivering human-centered solutions that benefit all stakeholders." srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
+            <SectionImage background="Vision" title="Our Vision" subtitle="To become a leading data and artificial intelligence (AI) technology company, excelling in the implementation of products based on academic research, creating impactful social analytics solutions, enabling data-driven decision-making, contributing to the development of digital talent and future technologies, and evolving into a funding company that supports innovation and the growth of data-driven startups." srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
+            <SectionImage imageRight={true} background="Mission" title="Our Mission"  
+            subtitle={
+                <ol className="list-decimal pl-5 space-y-2">
+                    <li>
+                    Develop accurate, reliable, and innovative social analytics products powered by data and 
+                    artificial intelligence (AI) to meet the needs of businesses, government, and society.
+                    </li>
+                    <li>
+                    Transform academic research into practical, market-ready products that deliver real value 
+                    and sustainable impact.
+                    </li>
+                    <li>
+                    Support research and product development for data-driven startups through strategic funding 
+                    and collaborative partnerships.
+                    </li>
+                    <li>
+                    Bridge the gap between academia and industry by providing opportunities for students, 
+                    researchers, and graduates to actively contribute to technology development.
+                    </li>
+                    <li>
+                    Foster a culture of innovation, collaboration, and adaptability to stay ahead in a 
+                    rapidly evolving technological landscape.
+                    </li>
+                </ol>
+                }
+            srcImage="https://assets.aceternity.com/templates/startup-2.webp" altImage="Vision Image"/>
             <SectionTimeline/>
         </>
     )
