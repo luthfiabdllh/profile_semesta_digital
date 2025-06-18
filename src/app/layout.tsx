35625dd77837eaa84fd6fe0c36567deb709a1486
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-06-18 12:11:38 +0700

    fix: fix favico conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,9 @@ export const metadata: Metadata = {
   keywords: ["big data", "artificial intelligence", "digital transformation", "data analytics", "ICT solutions", "layanan digital", "AI Indonesia"],
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/site-favicon.ico', sizes: 'any' },
+      { url: '/site-favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/site-favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
